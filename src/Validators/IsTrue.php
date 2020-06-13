8f26bd4276287bbf20399fc443371a815c2dcce9
papparazzo <pappi-@gmx.de>
2020-06-14 00:39:09 +0200

    isTrue-Validator added

---
+++ src/Validators/IsTrue.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ *  Project:    sfw2-validator
+ *
+ *  Copyright (C) 2019 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+namespace SFW2\Validator\Validators;
+
+use SFW2\Validator\Exception as ValidatorException;
+
+class IsTrue extends IsBool {
+
+    public function validate(string $value) : string {
+        $value = parent::validate($value);
+
+        if($value == '0') {
+            throw new ValidatorException('Der Haken muss gesetzt sein.');
+        }
+        return $value;
+    }
+}
