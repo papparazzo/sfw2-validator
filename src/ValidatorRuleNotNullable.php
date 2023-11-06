ec3e3aeb1c2a0446554c642fa2d49ac2dca3c373
papparazzo <pappi-@gmx.de>
2023-11-06 18:32:39 +0100

    preventing null in trim

---
+++ src/ValidatorRuleNotNullable.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2023  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace SFW2\Validator;
+
+abstract class ValidatorRuleNotNullable extends ValidatorRule
+{
+
+    public function validateNullable(?string $value): string
+    {
+        $value ??= '';
+        return $this->validate($value);
+    }
+
+    abstract function validate(string $value): string;
+}
