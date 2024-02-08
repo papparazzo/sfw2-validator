ab2140a8419e894ebaf01d3819240332ce92102f
papparazzo <pappi-@gmx.de>
2024-02-08 21:42:26 +0100

    IsNotSameAs added

---
+++ src/Validators/IsNotSameAs.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ *  Project:    sfw2-validator
+ *
+ *  Copyright (C) 2019 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace SFW2\Validator\Validators;
+
+use SFW2\Validator\Exception as ValidatorException;
+use SFW2\Validator\ValidatorRuleNotNullable;
+
+final class IsNotSameAs extends ValidatorRuleNotNullable
+{
+    public function __construct(
+        private readonly string $value,
+        private readonly bool $trim = true
+    )
+    {
+    }
+
+    /**
+     * @throws ValidatorException
+     */
+    public function validate(string $value): string
+    {
+        if ($this->trim) {
+            $value = trim($value);
+        }
+
+        if ($value == $this->value) {
+            throw new ValidatorException('Die Felder stimmen überein.');
+        }
+        return $value;
+    }
+}
