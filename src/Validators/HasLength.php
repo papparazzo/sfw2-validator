57dceacc70f23d5bfc9d8e2e47130ab2825f5d6c
papparazzo <pappi-@gmx.de>
2024-09-23 19:31:34 +0200

    HasLength added

---
+++ src/Validators/HasLength.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2024  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace SFW2\Validator\Validators;
+
+use SFW2\Validator\Exception as ValidatorException;
+use SFW2\Validator\ValidatorRuleNotNullable;
+
+class HasLength extends ValidatorRuleNotNullable
+{
+    public function __construct(
+        private readonly int $length,
+        private readonly bool $trim = true
+    )
+    {
+    }
+
+    /**
+     * @throws ValidatorException
+     */
+    public function validate(string $value): string
+    {
+        if ($this->trim) {
+            $value = trim($value);
+        }
+
+        if (mb_strlen($value) !== $this->length) {
+            throw new ValidatorException($this->replaceIn("Der Inhalt muss exakt {MAX} Zeichen lang sein.", ['MAX' => $this->length]));
+        }
+        return $value;
+    }
+}
